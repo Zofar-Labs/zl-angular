7b7cf1d88cab44f863f6d7dc5fce13f14b8e0fb2
Miguel Mateo <miguelmateot94@gmail.com>
2026-03-30 14:38:41 -0400

    feat(core): install core dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+import {
+  provideTanStackQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHotToastConfig(),
+    provideHttpClient(withFetch()),
+    provideTanStackQuery(new QueryClient()),
   ]
 };
