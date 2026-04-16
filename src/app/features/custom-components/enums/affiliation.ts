ea56aeddc7c3d43e3f13ffcfd09b62b7f00c4d2b
Miguel Mateo Tavarez <miguelmateot94@gmail.com>
2026-04-16 16:19:33 -0400

    fix(enums): correct typos and normalize strings in dragon ball enums

---
+++ src/app/features/custom-components/enums/affiliation.ts
@@ -0,0 +1,9 @@
+export enum Affiliation {
+  Z_FIGHTER = 'Z Fighter',
+  RED_RIBBON_ARMY = 'Red Ribbon Army',
+  NAMEKIAN_WARRIOR_FREELANCER = 'Namekian Warrior Freelancer',
+  ARMY_OF_FRIEZA_PRIDE_TROOPERS = 'Army of Frieza Pride Troopers',
+  ASSISTANT_OF_VERMOUD = 'Assistant of Vermoud',
+  GOD_ASSISTANT_OF_BEERUS_VILLAIN = 'God Assistant of Beerus Villain',
+  OTHER = 'Other',
+}
